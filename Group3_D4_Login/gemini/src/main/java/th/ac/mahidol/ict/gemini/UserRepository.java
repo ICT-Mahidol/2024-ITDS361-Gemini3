1a90c6164a52c305ba0126c65029fb4c88cebb3b
Natawat.tep <108444468+allinaV6@users.noreply.github.com>
2025-04-02 23:00:02 +0700

    Group3_D4_Login

---
+++ Group3_D4_Login/gemini/src/main/java/th/ac/mahidol/ict/gemini/UserRepository.java
@@ -0,0 +1,7 @@
+package th.ac.mahidol.ict.gemini;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
